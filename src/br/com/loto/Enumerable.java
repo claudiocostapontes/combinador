989d6979ecbf9d3cab5fc7081f8494170c41a32a
claudiocostapontes <claudionor.pontes@outlook.com>
2025-03-20 15:20:33 -0300

    Quinto Commit

--- src/br/com/loto/Enumerable.java
+++
@@ -1,9 +0,0 @@
-package br.com.loto;
-
-public class Enumerable {
-
-	public static Object Repeat(int i, int cnt) {
-		return null;
-	}
-
-}
--- src/br/com/loto/readonly.java
+++
@@ -1,5 +0,0 @@
-package br.com.loto;
-
-public class readonly {
-
-}
